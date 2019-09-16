0a68c40be4f87b18057bb13e739dbce7ce07d936
jose restrepo <arturo.restrepo@gmail.com>
2019-09-16 11:47:07 -0500

    Enabled server

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
-
+const bcrypt = require('bcryptjs');
 const db = require('./database/dbConfig.js');
 const Users = require('./users/users-model.js');
-
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
-
 server.get('/', (req, res) => {
-  res.send("It's alive!");
+  res.send("It's working!!!");
 });
-
 server.post('/api/register', (req, res) => {
   let user = req.body;
-
   Users.add(user)
     .then(saved => {
       res.status(201).json(saved);
@@ -26,10 +22,8 @@ server.post('/api/register', (req, res) => {
       res.status(500).json(error);
     });
 });
-
 server.post('/api/login', (req, res) => {
   let { username, password } = req.body;
-
   Users.findBy({ username })
     .first()
     .then(user => {
@@ -43,7 +37,6 @@ server.post('/api/login', (req, res) => {
       res.status(500).json(error);
     });
 });
-
 server.get('/api/users', (req, res) => {
   Users.find()
     .then(users => {
@@ -51,6 +44,14 @@ server.get('/api/users', (req, res) => {
     })
     .catch(err => res.send(err));
 });
+
+server.get('/hash', (req, res) => {
+  const name = req.query.name;
+
+  const hash = bcrypt.hashSync(name, 8);
+  res.send(`the hash for ${name} is ${hash}`)
+})
+
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
